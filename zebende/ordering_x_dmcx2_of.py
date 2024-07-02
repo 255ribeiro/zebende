99b59a891465af448df5deae3f458d461b9331ec
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2024-07-02 20:48:08 -0300

    ordering_x_dmcx2_of test passed

--- zebende/ordering_x_dmcx2_of.py
+++ zebende/ordering_x_dmcx2_of.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 def ordering_x_dmcx2_of(dmcx2_of):
-    y_series = dmcx2_of[0, :]
-    x_serie = dmcx2_of[1:, :]
-    out = np.concat(np.sort(x_serie, axis=1), y_series, axis=1)
-    return out
+    y_serie = dmcx2_of[:,0]
+    x_series = np.sort(dmcx2_of[:, 1:], axis=1)
+    print(x_series, y_serie)
+    out =  np.c_[ y_serie, x_series ] 
+    return out 
